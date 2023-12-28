a36c67f53baddbe60041b2871ad9ae0e66a9f621
SOURADEEP HAZRA <105676331+Souradeephazra123@users.noreply.github.com>
2023-12-28 15:47:29 +0530

    Update heading.jsx

--- src/Components/heading.jsx
+++ src/Components/heading.jsx
@@ -4,6 +4,7 @@ const heading = () => {
   return (
     <div>
       <p className=" text-center text-3xl">I am Souradeep Hazra</p>
+      <p className=" text-center text-3xl">I am Souradeep Hazra from uluberia</p>
     </div>
   );
 };
